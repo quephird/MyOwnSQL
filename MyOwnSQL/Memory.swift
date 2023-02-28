545eb6dae7911d135f473a6efc936f9543a478bc
quephird <danielle_kefford@hotmail.com>
2023-02-28 15:37:48 -0800

    First veryy rough draft of a working REPL.

--- MyOwnSQL/Memory.swift
+++ MyOwnSQL/Memory.swift
@@ -145,31 +145,38 @@ class MemoryBackend {
             }
         }
 
-        for tableRow in table.data {
+        for (rowNumber, tableRow) in table.data.enumerated() {
             var resultRow: [MemoryCell] = []
 
             for (i, item) in select.items.enumerated() {
                 switch item {
                 case .literal(let token):
-                    var newColumn: Column
                     switch token.kind {
                     case .boolean:
-                        newColumn = Column("col_\(i)", .boolean)
-                        columns.append(newColumn)
+                        if rowNumber == 0 {
+                            let newColumn = Column("col_\(i)", .boolean)
+                            columns.append(newColumn)
+                        }
                         resultRow.append(makeMemoryCell(token)!)
                     case .numeric:
-                        newColumn = Column("col_\(i)", .int)
-                        columns.append(newColumn)
+                        if rowNumber == 0 {
+                            let newColumn = Column("col_\(i)", .int)
+                            columns.append(newColumn)
+                        }
                         resultRow.append(makeMemoryCell(token)!)
                     case .string:
-                        newColumn = Column("col_\(i)", .text)
-                        columns.append(newColumn)
+                        if rowNumber == 0 {
+                            let newColumn = Column("col_\(i)", .text)
+                            columns.append(newColumn)
+                        }
                         resultRow.append(makeMemoryCell(token)!)
                     case .identifier(let requestedColumnName):
                         for (i, columnName) in table.columnNames.enumerated() {
                             if requestedColumnName == columnName {
-                                newColumn = Column(requestedColumnName, table.columnTypes[i])
-                                columns.append(newColumn)
+                                if rowNumber == 0 {
+                                    let newColumn = Column(requestedColumnName, table.columnTypes[i])
+                                    columns.append(newColumn)
+                                }
                                 resultRow.append(tableRow[i])
                                 break
                             }
--- MyOwnSQL/main.swift
+++ MyOwnSQL/main.swift
@@ -7,5 +7,79 @@
 
 import Foundation
 
-print("Hello, World!")
+let database = MemoryBackend()
+print("Welcome to MyOwnSQL!\n")
 
+while true {
+    print("SQL> ", terminator: "")
+    let input = readLine()
+
+    if input!.isEmpty {
+        print("Please enter a statement")
+        continue
+    }
+
+    switch parse(input!) {
+    case .failure(let errorMessage):
+        print(errorMessage)
+    case .success(let statements):
+        for statement in statements {
+            switch statement {
+            case .create(let createStatement):
+                do {
+                    try database.createTable(createStatement)
+                    print("Table created!")
+                } catch StatementError.misc(let errorMessage) {
+                    print(errorMessage)
+                }
+            case .insert(let insertStatement):
+                do {
+                    try database.insertTable(insertStatement)
+                    print("One row inserted!")
+                } catch StatementError.tableDoesNotExist {
+                    print("Table does not exist")
+                } catch StatementError.misc(let errorMessage) {
+                    print(errorMessage)
+                }
+            case .select(let selectStatement):
+                do {
+                    let results = try database.selectTable(selectStatement)
+                    if results.rows.isEmpty {
+                        print("No rows selected")
+                    } else {
+                        var columnHeader = ""
+                        for column in results.columns {
+                            columnHeader.append("| \(column.name) ")
+                        }
+                        columnHeader.append("|")
+                        print(columnHeader)
+                        let separator = String(repeating: "=", count: columnHeader.count)
+                        print(separator)
+
+                        for row in results.rows {
+                            var rowLine = ""
+                            for columnValue in row {
+                                switch columnValue {
+                                case .intValue(let integer):
+                                    rowLine.append("| \(integer) ")
+                                case .textValue(let string):
+                                    rowLine.append("| \(string) ")
+                                case .booleanValue(let boolean):
+                                    rowLine.append("| \(boolean) ")
+                                }
+                            }
+                            rowLine.append("|")
+                            print(rowLine)
+                        }
+                    }
+                } catch StatementError.tableDoesNotExist {
+                    print("Table does not exist")
+                } catch StatementError.columnDoesNotExist {
+                    print("Column does not exist")
+                } catch StatementError.misc(let errorMessage) {
+                    print(errorMessage)
+                }
+            }
+        }
+    }
+}
