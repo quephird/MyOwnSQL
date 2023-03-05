b54a75660d9fedf0464196a6669b15f248414399
quephird <danielle_kefford@hotmail.com>
2023-03-05 14:16:09 -0800

    Some refactoring

--- MyOwnSQL/main.swift
+++ MyOwnSQL/main.swift
@@ -26,60 +26,78 @@ while true {
         for statement in statements {
             switch statement {
             case .create(let createStatement):
-                do {
-                    try database.createTable(createStatement)
-                    print("Table created!")
-                } catch StatementError.misc(let errorMessage) {
-                    print(errorMessage)
-                }
+                handleCreateTable(createStatement)
             case .insert(let insertStatement):
-                do {
-                    try database.insertTable(insertStatement)
-                    print("One row inserted!")
-                } catch StatementError.tableDoesNotExist {
-                    print("Table does not exist")
-                } catch StatementError.misc(let errorMessage) {
-                    print(errorMessage)
-                }
+                handleInsertTable(insertStatement)
             case .select(let selectStatement):
-                do {
-                    let results = try database.selectTable(selectStatement)
-                    if results.rows.isEmpty {
-                        print("No rows selected")
-                    } else {
-                        var columnHeader = ""
-                        for column in results.columns {
-                            columnHeader.append("| \(column.name) ")
-                        }
-                        columnHeader.append("|")
-                        print(columnHeader)
-                        let separator = String(repeating: "=", count: columnHeader.count)
-                        print(separator)
-
-                        for row in results.rows {
-                            var rowLine = ""
-                            for columnValue in row {
-                                switch columnValue {
-                                case .intValue(let integer):
-                                    rowLine.append("| \(integer) ")
-                                case .textValue(let string):
-                                    rowLine.append("| \(string) ")
-                                case .booleanValue(let boolean):
-                                    rowLine.append("| \(boolean) ")
-                                }
-                            }
-                            rowLine.append("|")
-                            print(rowLine)
-                        }
-                    }
-                } catch StatementError.tableDoesNotExist {
-                    print("Table does not exist")
-                } catch StatementError.columnDoesNotExist {
-                    print("Column does not exist")
-                } catch StatementError.misc(let errorMessage) {
-                    print(errorMessage)
-                }
+                handleSelectTable(selectStatement)
             }
         }
+    }
+}
+
+func handleCreateTable(_ statement: CreateStatement) {
+    do {
+        try database.createTable(statement)
+        print("Table created!")
+    } catch StatementError.misc(let errorMessage) {
+        print(errorMessage)
+    } catch {
+        print(error.localizedDescription)
+    }
+}
+
+func handleInsertTable(_ statement: InsertStatement) {
+    do {
+        try database.insertTable(statement)
+        print("One row inserted!")
+    } catch StatementError.tableDoesNotExist {
+        print("Table does not exist")
+    } catch StatementError.misc(let errorMessage) {
+        print(errorMessage)
+    } catch {
+        print(error.localizedDescription)
+    }
+}
+
+func handleSelectTable(_ statement: SelectStatement) {
+    do {
+        let results = try database.selectTable(statement)
+        if results.rows.isEmpty {
+            print("No rows selected")
+        } else {
+            var columnHeader = ""
+            for column in results.columns {
+                columnHeader.append("| \(column.name) ")
+            }
+            columnHeader.append("|")
+            print(columnHeader)
+            let separator = String(repeating: "=", count: columnHeader.count)
+            print(separator)
+
+            for row in results.rows {
+                var rowLine = ""
+                for columnValue in row {
+                    switch columnValue {
+                    case .intValue(let integer):
+                        rowLine.append("| \(integer) ")
+                    case .textValue(let string):
+                        rowLine.append("| \(string) ")
+                    case .booleanValue(let boolean):
+                        rowLine.append("| \(boolean) ")
+                    }
+                }
+                rowLine.append("|")
+                print(rowLine)
+            }
+        }
+    } catch StatementError.tableDoesNotExist {
+        print("Table does not exist")
+    } catch StatementError.columnDoesNotExist {
+        print("Column does not exist")
+    } catch StatementError.misc(let errorMessage) {
+        print(errorMessage)
+    } catch {
+        print(error.localizedDescription)
     }
 }
