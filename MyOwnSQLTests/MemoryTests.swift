a20630a8b6cda0d94220619415f1388dccaf9d3a
quephird <danielle_kefford@hotmail.com>
2023-05-04 17:51:17 -0700

    Added more tests.

--- MyOwnSQLTests/MemoryTests.swift
+++ MyOwnSQLTests/MemoryTests.swift
@@ -446,6 +446,117 @@ class MemoryTests: XCTestCase {
         XCTAssertEqual(actualIds, expectedIds)
     }
 
+    func testSelectWithOneOrderByClauseExpression() throws {
+        let database = MemoryBackend()
+        let setup = """
+CREATE TABLE parts(id INT NOT NULL, name TEXT NOT NULL, color TEXT NOT NULL, weight INT NOT NULL, city TEXT NOT NULL);
+INSERT INTO parts VALUES(1, 'Nut', 'Red', 12, 'London');
+INSERT INTO parts VALUES(2, 'Bolt', 'Green', 17, 'Paris');
+INSERT INTO parts VALUES(3, 'Screw', 'Blue', 17, 'Oslo');
+INSERT INTO parts VALUES(4, 'Screw', 'Red', 14, 'London');
+INSERT INTO parts VALUES(5, 'Cam', 'Blue', 12, 'Paris');
+INSERT INTO parts VALUES(6, 'Cog', 'Red', 19, 'London');
+"""
+        let _ = database.executeStatements(setup)
+
+        let select = "SELECT * FROM parts ORDER BY id;"
+        let results = database.executeStatements(select)
+        guard let result = results.first else {
+            XCTFail("Something unexpected happened")
+            return
+        }
+        guard case .successfulSelect(let resultSet) = result else {
+            XCTFail("Something unexpected happened")
+            return
+        }
+
+        let expectedIds: [MemoryCell] = [
+            .intValue(1),
+            .intValue(2),
+            .intValue(3),
+            .intValue(4),
+            .intValue(5),
+            .intValue(6),
+        ]
+        let actualIds = resultSet.rows.map { row in
+            return row[0]
+        }
+        XCTAssertEqual(actualIds, expectedIds)
+    }
+
+    func testSelectWithMultipleOrderByClauseExpressions() throws {
+        let database = MemoryBackend()
+        let setup = """
+CREATE TABLE parts(id INT NOT NULL, name TEXT NOT NULL, color TEXT NOT NULL, weight INT NOT NULL, city TEXT NOT NULL);
+INSERT INTO parts VALUES(1, 'Nut', 'Red', 12, 'London');
+INSERT INTO parts VALUES(2, 'Bolt', 'Green', 17, 'Paris');
+INSERT INTO parts VALUES(3, 'Screw', 'Blue', 17, 'Oslo');
+INSERT INTO parts VALUES(4, 'Screw', 'Red', 14, 'London');
+INSERT INTO parts VALUES(5, 'Cam', 'Blue', 12, 'Paris');
+INSERT INTO parts VALUES(6, 'Cog', 'Red', 19, 'London');
+"""
+        let _ = database.executeStatements(setup)
+
+        let select = "SELECT * FROM parts ORDER BY city, name;"
+        let results = database.executeStatements(select)
+        guard let result = results.first else {
+            XCTFail("Something unexpected happened")
+            return
+        }
+        guard case .successfulSelect(let resultSet) = result else {
+            XCTFail("Something unexpected happened")
+            return
+        }
+
+        let expectedIds: [MemoryCell] = [
+            .intValue(6),
+            .intValue(1),
+            .intValue(4),
+            .intValue(3),
+            .intValue(2),
+            .intValue(5),
+        ]
+        let actualIds = resultSet.rows.map { row in
+            return row[0]
+        }
+        XCTAssertEqual(actualIds, expectedIds)
+    }
+
+    func testSelectWithWhereClauseAndOrderByClause() throws {
+        let database = MemoryBackend()
+        let setup = """
+CREATE TABLE parts(id INT NOT NULL, name TEXT NOT NULL, color TEXT NOT NULL, weight INT NOT NULL, city TEXT NOT NULL);
+INSERT INTO parts VALUES(1, 'Nut', 'Red', 12, 'London');
+INSERT INTO parts VALUES(2, 'Bolt', 'Green', 17, 'Paris');
+INSERT INTO parts VALUES(3, 'Screw', 'Blue', 17, 'Oslo');
+INSERT INTO parts VALUES(4, 'Screw', 'Red', 14, 'London');
+INSERT INTO parts VALUES(5, 'Cam', 'Blue', 12, 'Paris');
+INSERT INTO parts VALUES(6, 'Cog', 'Red', 19, 'London');
+"""
+        let _ = database.executeStatements(setup)
+
+        let select = "SELECT * FROM parts WHERE city = 'London' ORDER BY weight;"
+        let results = database.executeStatements(select)
+        guard let result = results.first else {
+            XCTFail("Something unexpected happened")
+            return
+        }
+        guard case .successfulSelect(let resultSet) = result else {
+            XCTFail("Something unexpected happened")
+            return
+        }
+
+        let expectedIds: [MemoryCell] = [
+            .intValue(1),
+            .intValue(4),
+            .intValue(6),
+        ]
+        let actualIds = resultSet.rows.map { row in
+            return row[0]
+        }
+        XCTAssertEqual(actualIds, expectedIds)
+    }
+
     func testSelectFailsForBadExpressionInSelectClause() throws {
         let database = MemoryBackend()
         let create = "CREATE TABLE dresses (id int, description text, is_in_season boolean);"
@@ -537,6 +648,22 @@ class MemoryTests: XCTestCase {
 
         XCTAssertEqual(result, .failure(.invalidExpression))
     }
+
+    func testSelectFailsForOrderByClauseReferencingNonexistentColumn() throws {
+        let database = MemoryBackend()
+        let create = "CREATE TABLE dresses (id int, description text, is_fabulous boolean);"
+        let _ = database.executeStatements(create)
+
+        let select = "SELECT * FROM dresses ORDER BY is_velvet;"
+        let results = database.executeStatements(select)
+        guard let result = results.first else {
+            XCTFail("Something unexpected happened")
+            return
+        }
+
+        XCTAssertEqual(result, .failure(.columnDoesNotExist("is_velvet")))
+    }
+
 
     func testDeleteAllRows() throws {
         let database = MemoryBackend()
